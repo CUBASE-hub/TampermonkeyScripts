0720ea9a6fb4992746bf35eff5d0187388eb9b6d
CUBASE-hub <yonijmusic0@gmail.com>
2025-05-20 19:29:54 +0300

    Update and rename ‏‏Call2All Tzintukim Enhancer.meta.js to Yemot Tzintukim Enhancer.meta.js

---
+++ Yemot Tzintukim Enhancer.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Yemot Tzintukim Enhancer
+// @namespace    http://tampermonkey.net/
+// @version      1.6
+// @description  Adds "Invited to list" column, updates totals on Yemot Tzintukim page, and adds invited numbers to list entries page with table-specific loading animation.
+// @author       Jonny
+// @match        https://*.call2all.co.il/ym/index.php?view=Tzintukim
+// @match        https://*.call2all.co.il/ym/index.php?view=Tzintukim&action=getlistEnteres&list=*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @connect      self
+// @connect      *.call2all.co.il
+// @updateURL    https://github.com/CUBASE-hub/TampermonkeyScripts/raw/refs/heads/main/Yemot%20Tzintukim%20Enhancer.meta.js
+// @downloadURL  https://github.com/CUBASE-hub/TampermonkeyScripts/raw/refs/heads/main/Yemot%20Tzintukim%20Enhancer.user.js
+// ==/UserScript==
